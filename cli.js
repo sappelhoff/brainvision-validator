eb9e86e30fd3ad0c035a5db6c0e7a62b770627f9
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-12 14:04:40 +0200

    added verbose option

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 // API: Always accept a single input: a .vhdr header file
 var bvv = require('./index.js');
 
-module.exports = function(vhdrPath) {
+module.exports = function(vhdrPath, options) {
+    console.log(!options.verbose);
     console.log(bvv.assertBVTriplet(vhdrPath));
 };
