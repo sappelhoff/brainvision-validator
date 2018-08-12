29990607b25ce70faa6d349ee4ea7ac0c8fdf79e
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-12 17:10:59 +0200

    tests for main validator

---
+++ test/test_index.js
@@ -0,0 +1,18 @@
+var expect = require("chai").expect;
+var path = require("path");
+var validateBrainVision = require("../index.js").validateBrainVision;
+
+describe("validateBrainVision", function() {
+
+    it("returns an empty array for valid checks", function() {
+        var vhdrPath = path.join(__dirname, 'data/test.vhdr');
+        var issues = validateBrainVision(vhdrPath);
+        expect(issues).to.be.an('array').that.is.empty;
+    }),
+
+    it("returns an array of issues for invalid checks", function() {
+        var vhdrPath = path.join(__dirname, 'data/test_broken_link.vhdr');
+        var issues = validateBrainVision(vhdrPath);
+        expect(issues).to.be.an('array').that.is.not.empty;
+    })
+});
