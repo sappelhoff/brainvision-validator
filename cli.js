f1215acbd22de9f6e3edd960deaf1acf29358911
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-12 17:11:44 +0200

    command line interface now prints the issues

--- cli.js
+++ cli.js
@@ -1,8 +1,25 @@
 // Import the validator
 // API: Always accept a single input: a .vhdr header file
-var bvv = require('./index.js');
+var colors = require('colors');
+var validateBrainVision = require('./index.js').validateBrainVision;
 
 module.exports = function(vhdrPath, options) {
-    console.log(!options.verbose);
-    console.log(bvv.assertBVTriplet(vhdrPath));
+    var issues = validateBrainVision(vhdrPath);
+    if (issues.length == 0){
+
+        console.log('BrainVision file triplet is valid.'.green);
+
+    } else if (issues.length > 0 && options.verbose) {
+
+        console.log('BrainVision file triplet is invalid!'.red);
+        for (i = 0; i < issues.length; i++) {
+            console.log(issues[i].yellow);
+            console.log('');
+        }
+
+    } else {
+
+        console.log('BrainVision file triplet is invalid!'.red);
+        console.log('Use the --verbose flag to print out the issues.'.yellow);
+    }
 };
