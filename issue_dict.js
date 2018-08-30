d2ca46c78ba8a79066f3e8ba4d529cfa413a2305
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-30 11:17:20 +0200

    use dictionary of issues

---
+++ issue_dict.js
@@ -0,0 +1,10 @@
+/**
+ * issue_dict
+ *
+ * Dictionary of potential issues with the BrainVision files.
+ */
+module.exports = {
+    1: "Wrong file extension. Header file should end with .vhdr",
+    2: "Internal links are broken or files (.eeg, .vhdr, .vmrk)." +
+       " Check DataFile and MarkerFile fields in .vhdr and .vmrk",
+};
