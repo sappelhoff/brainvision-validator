fbbe578ac14ad850728d7aabef44dbbbeef930bd
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-30 11:17:57 +0200

    fix tests. fixes #3

--- test/test_validator.js
+++ test/test_validator.js
@@ -6,14 +6,14 @@ describe("assertIsVHDR", function(){
 
     it("detects wrong file extensions.", function() {
         var vhdrPath = path.join(__dirname, 'data/test.wrong');
-        var isVHDR = validator.assertIsVHDR(vhdrPath);
-        expect(isVHDR).to.equal(false);
+        var vhdrCheck = validator.assertIsVHDR(vhdrPath);
+        expect(vhdrCheck).to.equal(1);
     });
 
     it("detects correct file extensions.", function() {
         var vhdrPath = path.join(__dirname, 'data/test.vhdr');
-        var isVHDR = validator.assertIsVHDR(vhdrPath);
-        expect(isVHDR).to.equal(true);
+        var vhdrCheck = validator.assertIsVHDR(vhdrPath);
+        expect(vhdrCheck).to.equal(null);
     });
 });
 
@@ -21,19 +21,19 @@ describe("assertBVTriplet", function(){
 
     it("detects correct links.", function() {
         var vhdrPath = path.join(__dirname, 'data/test.vhdr');
-        var isTriplet = validator.assertBVTriplet(vhdrPath);
-        expect(isTriplet).to.equal(true);
+        var linkCheck = validator.assertBVTriplet(vhdrPath);
+        expect(linkCheck).to.equal(null);
     });
 
     it("detects inconsistent DataFile links in vhdr and vmrk.", function() {
         var vhdrPath = path.join(__dirname, 'data/test_broken_link.vhdr');
-        var isTriplet = validator.assertBVTriplet(vhdrPath);
-        expect(isTriplet).to.equal(false);
+        var linkCheck = validator.assertBVTriplet(vhdrPath);
+        expect(linkCheck).to.equal(2);
     });
 
     it("detects wrong links.", function() {
         var vhdrPath = path.join(__dirname, 'data/test_broken_link2.vhdr');
-        var isTriplet = validator.assertBVTriplet(vhdrPath);
-        expect(isTriplet).to.equal(false);
+        var linkCheck = validator.assertBVTriplet(vhdrPath);
+        expect(linkCheck).to.equal(2);
     });
 });
